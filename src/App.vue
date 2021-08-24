6e779fa53f916999ad1522a879c5d6322ca555eb
liuqh <liuqinghua.lqh@alibaba-inc.com>
2021-08-24 11:15:56 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div  style="border: 1px solid #ccc; margin-top: 10px;height: 500px">
+    <Editor :editorId="editorId"  :defaultConfig="defaultConfig" :defaultContent="defaultContent" @onCreated="handleCreated"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Editor from './components/Editor.vue'
+import { SlateEditor } from '@wangeditor/editor-cattle'
+
+export default defineComponent({
+  name: 'App',
+  data(){
+    return{
+      defaultContent: [{ type: 'paragraph', children: [{ text: 'basic demo' }] }],
+      defaultConfig: {},
+      mode:'default',
+      editorId: 'we-1001'
+    }
+  },
+  components: {
+    Editor
+  },
+  methods:{
+    handleCreated(editor: SlateEditor){
+      console.log('%c 🍸 editor: ', 'font-size:20px;background-color: #465975;color:#fff;', editor);
+    }
+  }
+})
+</script>
+
+
